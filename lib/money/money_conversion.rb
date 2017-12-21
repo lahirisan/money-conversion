50cd4862b428189491583fda5df7d0d42dd3c5cb
Lahiri San <lahiri.sanchez@gmail.com>
2017-12-21 17:09:44 -0300

    Método para dividir y multiplicar monedas, Se permite dividir y multiplicar moneda enrre un número, monedas entre monedas,  bien sea sel mismo tipo ó monedas distintas y en caso de distintas. Se hace la conversión al operando de más a la izquierda

--- lib/money/money_conversion.rb
+++ lib/money/money_conversion.rb
@@ -39,7 +39,7 @@ class MoneyConversion
         puts "Excepción de division entre 0"
       end
   end
-  
+
   def + (currency_object)
       
       MoneyConversion.new(self.amount + currency_object.convert_to(self.default_currency).amount, self.default_currency)
@@ -48,6 +48,45 @@ class MoneyConversion
   def - (currency_object)
     
     MoneyConversion.new(self.amount - currency_object.convert_to(self.default_currency).amount, self.default_currency)
+  end
+
+  def / (divisor)
+
+    begin
+
+      if divisor.class.name == self.class.name
+        if  (divisor.default_currency == self.default_currency)
+          div = self.amount.to_f / divisor.amount
+          result = MoneyConversion.new(div, self.default_currency)
+        else
+          converted = divisor.convert_to(self.default_currency)
+          result = MoneyConversion.new(self.amount.to_f / converted.amount, self.default_currency)
+        end
+      else
+        amount = self.amount.to_f / divisor
+        result = MoneyConversion.new(amount, self.default_currency)
+      end
+      result
+    
+    rescue ZeroDivisionError => b
+      puts "Excepción de division entre 0"
+    end
+
+  end
+
+  def * (factor)
+    if factor.class.name == self.class.name
+      if  (factor.default_currency == self.default_currency)
+        div = self.amount.to_f * factor.amount
+        result = MoneyConversion.new(div, self.default_currency)
+      else
+        converted = factor.convert_to(self.default_currency)
+        result = MoneyConversion.new(self.amount.to_f * converted.amount, self.default_currency)
+      end
+    else
+      amount = self.amount.to_f * factor
+      result = MoneyConversion.new(amount, self.default_currency)
+    end
   end
 
 end
